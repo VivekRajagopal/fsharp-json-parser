503d6dd6323e3871b5c5f15af1e851ebc85e3aa4
Vivek Rajagopal <vivrajagopal@gmail.com>
2021-10-25 11:21:10 +1100

    Test new CI

--- src/Parser.WebUI/src/App.fs
+++ src/Parser.WebUI/src/App.fs
@@ -3,18 +3,19 @@ module App
 open Browser.Dom
 open Parser
 
-// Mutable variable to count the number of times we clicked the button
 let mutable parsedJson: Result<JValue * string, string> = Error "Uninitialised"
 
-// Get a reference to our button and cast the Element to an HTMLButtonElement
 let unparsedJsonInput = document.getElementById("raw-text-input") :?> Browser.Types.HTMLTextAreaElement
 let parsedJsonOutput = document.getElementById("parsed-json") :?> Browser.Types.HTMLTextAreaElement
 
-// Register our listener
-unparsedJsonInput.oninput <- fun _ev ->
+let parseRawText () = 
     parsedJson <- Parser.run JsonParsing.JSON.parser unparsedJsonInput.value
     match parsedJson with
-    | Ok (jvalue, remaining) -> 
-        parsedJsonOutput.innerText <- sprintf "%A" jvalue
+    | Ok (jvalue, _remaining) -> 
+        parsedJsonOutput.innerText <- sprintf $"""%A{jvalue}"""
     | Error error ->
         parsedJsonOutput.innerText <- sprintf "ERROR: %A" error
+
+unparsedJsonInput.oninput <- fun _ev -> parseRawText()
+
+parseRawText()
