07ff65bf641fd497bbdbe710515869bcd4105a33
Vivek Rajagopal <vivrajagopal@gmail.com>
2021-10-26 16:07:30 +1100

    Better json rendering in webui

--- src/Parser.WebUI/src/App.fs
+++ src/Parser.WebUI/src/App.fs
@@ -8,11 +8,48 @@ let mutable parsedJson: Result<JValue * string, string> = Error "Uninitialised"
 let unparsedJsonInput = document.getElementById("raw-text-input") :?> Browser.Types.HTMLTextAreaElement
 let parsedJsonOutput = document.getElementById("parsed-json") :?> Browser.Types.HTMLTextAreaElement
 
+let rec renderValue indentationLevel value =
+  let indent level =
+    let twoSpaces = "  "
+    String.replicate level twoSpaces
+
+  let reduceLines (lines: string list) =
+    match lines.Length with
+    | 0 -> ""
+    | _ -> lines |> List.reduce (fun a b -> $"""{a},
+{indent indentationLevel}{b}""")
+
+  match value with
+  | JNull -> "null"
+  | JBool bool -> string bool
+  | JNumber num -> string num
+  | JString str -> $"\"{str}\""
+  | JArray array ->
+    let lines = 
+      array
+      |> List.map (renderValue (indentationLevel + 1))
+      |> reduceLines
+     
+    $"""[
+{indent indentationLevel}{lines}
+{indent (indentationLevel - 1)}]"""
+
+  | JObject map -> 
+    let lines = 
+      map
+      |> Map.toList
+      |> List.map (fun (key, value) -> $"\"{key}\": {renderValue (indentationLevel + 1) value}")
+      |> reduceLines
+       
+    $"""{{
+{indent indentationLevel}{lines}
+{indent (indentationLevel - 1)}}}"""
+
 let parseRawText () = 
     parsedJson <- Parser.run JsonParsing.JSON.parser unparsedJsonInput.value
     match parsedJson with
     | Ok (jvalue, _remaining) -> 
-        parsedJsonOutput.innerText <- sprintf $"""%A{jvalue}"""
+        parsedJsonOutput.innerText <- renderValue 1 jvalue
     | Error error ->
         parsedJsonOutput.innerText <- sprintf "ERROR: %A" error
 
